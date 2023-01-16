249614080ae437d8e3fd2744de9189c4496ee051
imsamar <samar.joharchi@gmail.com>
2023-01-16 18:42:52 +0330

    added js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f422f06cd1f20109521ed5d7bf0e2910";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherCondition);
